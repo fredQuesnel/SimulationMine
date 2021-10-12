17dc6c1c3d67afe4f49c1fb67e8c8b3e0dae4251
Frederic Quesnel <frederic.quesnel@gerad.ca>
2021-10-12 12:18:48 -0400

    met le projet dans un état fonctionnel sous linux.

--- bin/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ca.polymtl.SimulationMine.decisionMaker"},{"l":"ca.polymtl.SimulationMine.MineGui"},{"l":"ca.polymtl.SimulationMine.MineSimulator"}];updateSearchResults();
