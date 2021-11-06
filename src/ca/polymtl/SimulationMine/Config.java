527a310b24f6dc2d87fd909644ac2cb1a8f92e62
Fred <Fred@Fred-PC>
2021-11-06 08:44:51 -0400

    fin config

--- src/ca/polymtl/SimulationMine/Config.java
+++ src/ca/polymtl/SimulationMine/Config.java
@@ -1,6 +1,9 @@
 package ca.polymtl.SimulationMine;
 
-
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Config {
 
@@ -22,55 +25,116 @@ public class Config {
 	private int defaultSimultaionSpeed;
 	/**Pause a la fin de voyage*/
 	private boolean defaultPauseFinVoyage;
-	
+
 	/**
 	 * Constructeur
 	 */
 	public Config() {
-		this.theme="espace";
-		this.defaultMineId="10pellessanspannes";
-		this.defaultScoreFunction="aleatoire";
-		this.defaultTimePredictFormula=1;
-		this.defaultTimePredictN=4;
-		this.defaultTimePredictLambda=0.5;
-		this.defaultMeteo=100;
-		this.defaultSimultaionSpeed=26;
-		this.defaultPauseFinVoyage=false;
+
+		try {
+			readConfigFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
-	public String getTheme() {
-		return theme;
-	}
+	private void readConfigFile() throws IOException {
 
-	public String getDefaultMineId() {
-		return defaultMineId;
-	}
+		String stringPattern = "\".*\"";
+		Scanner sc = new Scanner(new File("config"));
+		//pour que le point délimite la pratie fractionnaire
+		sc.useLocale(Locale.US);
+		
+		while(sc.hasNext()) {
+			String token = sc.next();
+			System.out.println(token);
+			sc.next(":");
 
-	public String getDefaultScoreFunction() {
-		return defaultScoreFunction;
+			switch(token){
+			case "theme":
+				String theme = sc.next(stringPattern);
+				this.theme= theme.substring(1, theme.length()-1);
+				break;
+			case "default_mine_id":
+				String defaultMineId = sc.next(stringPattern);
+				this.defaultMineId = defaultMineId.substring(1, defaultMineId.length()-1);
+				break;
+			case "default_score_function":
+				String defaultScoreFunction = sc.next(stringPattern);
+				this.defaultScoreFunction = defaultScoreFunction.substring(1, defaultScoreFunction.length()-1);
+				break;
+			case "default_time_predict_formula":
+				this.defaultTimePredictFormula = sc.nextInt();
+				break;
+			case "default_time_predict_n":
+				this.defaultTimePredictN = sc.nextInt();
+				break;
+			case "default_time_predict_lambda":
+				this.defaultTimePredictLambda = sc.nextDouble();
+				break;
+			case "default_simulation_speed":
+				this.defaultSimultaionSpeed = sc.nextInt();
+				break;
+			case "default_meteo" : 
+				System.out.println("ici");
+				this.defaultMeteo = sc.nextInt();
+				break;
+			case "default_pause_fin_voyage":
+				int val = sc.nextInt();
+				if(val == 1) {
+					this.defaultPauseFinVoyage = true;
+				}
+				else if (val == 0) {
+					this.defaultPauseFinVoyage = false;
+				}
+				else {
+					throw new IOException("le parametre \"default_pause_fin_voyage\" doit etre 0 ou 1");
+				}
+				break;
+			default : 
+				throw new IOException("je ne reconnais pas le parametre "+token);
+			}
+		}
+		sc.close();
 	}
+	
 
-	public int getDefaultTimePredictFormula() {
-		return defaultTimePredictFormula;
-	}
 
-	public int getDefaultTimePredictN() {
-		return defaultTimePredictN;
-	}
+public String getTheme() {
+	return theme;
+}
 
-	public double getDefaultTimePredictLambda() {
-		return defaultTimePredictLambda;
-	}
+public String getDefaultMineId() {
+	return defaultMineId;
+}
 
-	public int getDefaultMeteo() {
-		return defaultMeteo;
-	}
+public String getDefaultScoreFunction() {
+	return defaultScoreFunction;
+}
 
-	public int getDefaultSimultaionSpeed() {
-		return defaultSimultaionSpeed;
-	}
+public int getDefaultTimePredictFormula() {
+	return defaultTimePredictFormula;
+}
 
-	public boolean isDefaultPauseFinVoyage() {
-		return defaultPauseFinVoyage;
-	}
+public int getDefaultTimePredictN() {
+	return defaultTimePredictN;
+}
+
+public double getDefaultTimePredictLambda() {
+	return defaultTimePredictLambda;
+}
+
+public int getDefaultMeteo() {
+	return defaultMeteo;
+}
+
+public int getDefaultSimultaionSpeed() {
+	return defaultSimultaionSpeed;
+}
+
+public boolean isDefaultPauseFinVoyage() {
+	return defaultPauseFinVoyage;
+}
 }
